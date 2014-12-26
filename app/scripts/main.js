c2a07d87e48f424cc049eb97521401daafd53168
Ryan Wedig <ryan@activewebsite.com>
2014-12-25 19:50:23 -0800

    making it show more info

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,18 +5,20 @@ console.log('\'Allo \'Allo!');
 $(document).ready(function() {
 
   
-  var animateTo = 240;
+  var animateTo = 50;
+
+  animateTo = animateTo*.75;
 
   $('#liquid') // I Said Fill 'Er Up!
     .delay(1000)
     .animate({
-      height: animateTo + 'px'
+      height: animateTo + '%'
     }, 2500);
   
   $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
     .delay(1000)
     .animate({
-      bottom: (animateTo - 100) + 'px'
+      bottom: (animateTo - 15) + '%'
       }, 2500);
   });
 
