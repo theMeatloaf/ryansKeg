c8de294b24096b4e8393aaff56f56a7cf92d5028
Chris Chares <chris@eunoia.cc>
2014-12-25 20:00:51 -0800

    dynamic yo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,33 @@
 'use strict';
 
-console.log('\'Allo \'Allo!');
-
-$(document).ready(function() {
-
-  
-  var animateTo = 300;
-
+function animateToPoint(point) {
   $('#liquid') // I Said Fill 'Er Up!
     .delay(1000)
     .animate({
-      height: animateTo + 'px'
+      height: point + 'px'
     }, 2500);
   
   $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
     .delay(1000)
     .animate({
-      bottom: (animateTo - 100) + 'px'
+      bottom: (point - 100) + 'px'
       }, 2500);
-  });
+}
 
+// percent is (0.0-1.0)
+function animateToPercent(percent) {
+  var height = window.innerHeight;
+  var point = height * percent;
+  animateToPoint(point);
+}
+
+$(document).ready(function() {
+  changePercent();
+});
+
+
+function changePercent() {
+  var value = $('#temp').val();
+  animateToPercent(value);
+  console.log('value = ' + value);
+}
