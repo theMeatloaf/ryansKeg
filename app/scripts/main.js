8d0e64c651dcf7e613e167bc9402515eb4a65b96
Ryan Wedig <ryan@wedig.com>
2016-05-25 10:27:47 -0600

    took out parse gallery for now...about to move to firebase if possible

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,7 +15,7 @@ function getBeers()
       animateToBeers(value);
 
       //load images for gallery
-      loadGallery();
+      //loadGallery();
 
       //sparkcoreLogic.js to load all the other values
       loadAll();
@@ -66,7 +66,7 @@ function formatAMPM(date) {
 
 function animateToBeers(beersLeft) {
   var height = window.innerHeight;
-  var point = Math.max(150,(height*0.7) * (beersLeft / 53.0));
+  var point = Math.max(150,(height*0.7) * (beersLeft / 165.0));
   point = Math.min(point,height*0.62);
 
   $('#liquid') // I Said Fill 'Er Up!
